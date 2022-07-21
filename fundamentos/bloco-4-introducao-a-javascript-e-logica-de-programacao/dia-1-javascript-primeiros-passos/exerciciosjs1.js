b41c5495de887e69b9221bf0044618f6a6551e54
Joao Victor <imashinzo10@gmail.com>
2022-07-21 13:46:23 -0300

    Execicio 2 e 3 concluidos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exerciciosjs1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exerciciosjs1.js
@@ -8,3 +8,28 @@ console.log("Subtração: " + (a - b));
 console.log("Multiplicação: " + a * b);
 console.log("Divisão: " + a / b);
 console.log("Módulo: " + (a % b));
+
+//Exercicio 2 //
+
+const c = 20;
+const d = 21;
+
+if (c > d) {
+  console.log("'c' é maior que 'd'");
+} else {
+  console.log("'d' é maior que 'c'");
+}
+
+// Exercicio 3 //
+
+const e = 6;
+const f = 4;
+const g = 2;
+
+if (e > f && e > g) {
+  console.log("O maior número é: " + e + " (e)");
+} else if (f > e && f > g) {
+  console.log("O maior número é: " + f + " (f)");
+} else {
+  console.log("O maior número é: " + g + " (g)");
+}
