eff6d164f8e5ec6a2f27e9e098113f0084f1aaf0
Joao Victor <imashinzo10@gmail.com>
2022-07-25 11:51:53 -0300

    Exercicios 4 e 5 concluidos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios2.js
@@ -8,22 +8,50 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Exercicio 2 //
 
-let somaDosNumeros = 0;
+// let somaDosNumeros = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
-  somaDosNumeros += numbers[index];
-}
-console.log(somaDosNumeros);
+// for (let index = 0; index < numbers.length; index += 1) {
+//   somaDosNumeros += numbers[index];
+// }
+// console.log(somaDosNumeros);
 
 // Exercicio 3 //
 
-let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
+// let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
 
-for (let index = 0; index < numbers1.length; index += 1) {
-  sum += numbers1[index];
+// for (let index = 0; index < numbers1.length; index += 1) {
+//   sum += numbers1[index];
+// }
+
+// let average = sum / numbers1.length;
+
+// console.log(average);
+
+// Exercicio 4 //
+
+// let numbers1 = [5, 9, 3, 1, 1, 8, 1, 2, 3, 7];
+// let sum = 0;
+
+// for (let index = 0; index < numbers1.length; index += 1) {
+//   sum += numbers1[index];
+// }
+
+// let average = sum / numbers1.length;
+
+// if (average > 20) {
+//   console.log("Valor maior que 20");
+// } else {
+//   console.log("Valor menor que 20");
+// }
+
+// Exercicio 5 //
+
+let maiorNumero = numbers[0];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > maiorNumero) {
+    maiorNumero = numbers[index];
+  }
 }
-
-let average = sum / numbers1.length;
-
-console.log(average);
+console.log(maiorNumero);
