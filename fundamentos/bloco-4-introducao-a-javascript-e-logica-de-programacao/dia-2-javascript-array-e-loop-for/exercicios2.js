d8ac3b3db07455d99aaf54befe48f9fe453546a7
Joao Victor <imashinzo10@gmail.com>
2022-08-01 14:53:57 -0300

    Att 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios2.js
@@ -1,57 +1,83 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// Exercicio 1 //
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
 
-// for (let index = 0; index < numbers.length; index += 1) {
+// for (index in numbers) {
 //   console.log(numbers[index]);
 // }
 
-// Exercicio 2 //
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
 // let somaDosNumeros = 0;
-
-// for (let index = 0; index < numbers.length; index += 1) {
-//   somaDosNumeros += numbers[index];
+// for (soma of numbers) {
+//   somaDosNumeros += soma;
 // }
 // console.log(somaDosNumeros);
 
-// Exercicio 3 //
+// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 
-// let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// let sum = 0;
+// let mediaArray = 0;
+// let somaDosNumeros = 0;
 
-// for (let index = 0; index < numbers1.length; index += 1) {
-//   sum += numbers1[index];
+// for (media of numbers) {
+//   somaDosNumeros += media;
+// }
+// mediaArray = somaDosNumeros / numbers.length;
+
+// console.log(mediaArray);
+
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+// let mediaArray = 0;
+// let somaDosNumeros = 0;
+
+// for (media of numbers) {
+//   somaDosNumeros += media;
+// }
+// mediaArray = somaDosNumeros / numbers.length;
+
+// if (mediaArray >= 20) {
+//   console.log("valor maior que 20");
+// }
+// if (mediaArray < 20) {
+//   console.log("valor menor ou igual a 20");
 // }
 
-// let average = sum / numbers1.length;
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
 
-// console.log(average);
+// let maiorValor = 0;
 
-// Exercicio 4 //
+// for (num of numbers) {
+//   if (num > maiorValor) {
+//     maiorValor = num;
+//   }
+// }
+// console.log(maiorValor);
 
-// let numbers1 = [5, 9, 3, 1, 1, 8, 1, 2, 3, 7];
-// let sum = 0;
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
-// for (let index = 0; index < numbers1.length; index += 1) {
-//   sum += numbers1[index];
+// let oddNumbers = 0;
+
+// for (odds of numbers) {
+//   if (odds % 2 !== 0) {
+//     oddNumbers = odds;
+//     console.log(oddNumbers);
+//   } else if (oddNumbers === 0) {
+//     oddNumbers = "nenhum valor ímpar encontrado";
+//     console.log(oddNumbers);
+//   }
 // }
 
-// let average = sum / numbers1.length;
+// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
 
-// if (average > 20) {
-//   console.log("Valor maior que 20");
-// } else {
-//   console.log("Valor menor que 20");
-// }
+let menorValor = numbers[0];
 
-// Exercicio 5 //
-
-let maiorNumero = numbers[0];
-
-for (let index = 0; index < numbers.length; index += 1) {
-  if (numbers[index] > maiorNumero) {
-    maiorNumero = numbers[index];
+for (num of numbers) {
+  if (num < menorValor) {
+    menorValor = num;
   }
 }
-console.log(maiorNumero);
+console.log(menorValor);
+
+// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
